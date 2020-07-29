de15f7de4f625a423c939b48ed8337a05123ac12
Jessie <FranceJessieJ@gmail.com>
2020-07-29 11:48:13 -0600

    Added POST function for creating a new user.

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,36 @@ var User = require('../models/user')
 var async = require('async')
 
 
+// Create a new user.
+// Input stored in req.body will be {new_user: 'something typed into form'}
+exports.post_user = function(req, res, next){
 
+    // TODO: validate/sanitize data.
+
+    
+    // Create new user object.
+    var userObj = new User(
+	{user_name: req.body.new_user}
+    );
+
+    // Check if username exists in db ....
+    User.findOne({ 'user_name': req.body.new_user })
+        .exec( function(err, found_user) {
+            if (err) { return next(err); }
+            if (found_user) {
+		// User exists. Display that user exists.
+		res.send('Username is already taken.');
+	    }
+            else {
+		userObj.save(function (err) {
+		    if (err) { return next(err); }
+		    // User object saved. Display new user and id field.
+		    res.json({"username": userObj.user_name, "_id": userObj._id.toString() } );
+		});
+	    }
+	});
+    
+}
 
 // Display information for single user.
 exports.get_single_user = function (req, res, next) {
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -8,11 +8,15 @@ var user_controller = require('../controllers/userController');
 // -------------ROUTES-------------
 
 
-// GET request for single user.
+// Create or POST new username.
+// Must match action part in form.
+router.post('/exercise/new-user', user_controller.post_user);
+
+// Read single user's exercise log with GET request.
 router.get('/exercise/log', user_controller.get_single_user);
 
 
-// GET request for list of all users.
+// Read list of all users with GET request.
 router.get('/exercise/users', user_controller.user_list);
 
 // -------------END OF ROUTES-------------
